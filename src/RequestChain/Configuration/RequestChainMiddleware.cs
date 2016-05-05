ae3b45616d2e88c8b993ff09e02d732af1df5539
cbeall <cbeall@users.noreply.github.com>
2016-05-05 07:25:15 -0400

    Added comments to RequestChainMiddleware

--- src/RequestChain/Configuration/RequestChainMiddleware.cs
+++ src/RequestChain/Configuration/RequestChainMiddleware.cs
@@ -24,34 +24,38 @@ namespace RequestChain.Configuration
 
         public async Task Invoke(HttpContext context, IRequestId requestId, RequestChainOptions options)
         {
-            RequestId settableRequestId = requestId as RequestId;
-
-            settableRequestId.SetRequestId(context.Request, options);
-
             DateTime start = DateTime.Now;
 
-            string requestDepth = string.Empty;
+            // Gets the request scoped IRequestId as the underlying class to have access to internal set methods
+            RequestId settableRequestId = requestId as RequestId;
 
+            // Set the request id based on the current request context
+            settableRequestId.SetRequestId(context.Request, options);
+
+            // Get string formatted depth for logging
+            string requestDepth = string.Empty;
             if (options.IncludeRequestDepth && settableRequestId.HasValidDepth)
             {
                 requestDepth = $"(Depth {requestId.Depth}) ";
             }
 
+            // Log start of request
             _logger.Log(options.RequestBeginEndLogLevel,
                 "Begin request {0} {1}for {2} on {3}", 
                 requestId.Value, requestDepth, context.Request.Path, context.Request.PathBase);
                 
+            // Execute actual request logic
             await _next.Invoke(context);
 
-            TimeSpan requestTime = DateTime.Now - start;
-
+            // Get status code for logging
             string statusCodeStr = string.Empty;
-            
             if (context.Response.StatusCode != default(int))
             {
                 statusCodeStr = $"with status code {context.Response.StatusCode} ";
             }
 
+            // Log end of request
+            TimeSpan requestTime = DateTime.Now - start;
             _logger.Log(options.RequestBeginEndLogLevel, "End request {0} {2}({1} ms)", 
                 requestId.Value, requestTime.Milliseconds, statusCodeStr);
         }
